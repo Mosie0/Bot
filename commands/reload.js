ae1fe99857696aa4177ce96d5d2da5699255a65e
Jonbn123 <42418062+Jonbn123@users.noreply.github.com>
2018-08-15 14:26:43 -0700

    Add files via upload

--- commands/reload.js
+++ commands/reload.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args, ops) => {
 
-if (message.author.id !== ops.ownerID) return message.channel.send('Sorry, only the owner can use this command.');
+if (message.author.id !== '244271175608303616') return message.channel.send('Sorry, only the owner can use this command.');
 
 try {
     delete require.cache[require.resolve(`./{args[0]}.js`)];
