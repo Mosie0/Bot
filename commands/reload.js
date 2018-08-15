beb2ff52ebcaf7b8e7de2a9dfe8be88fa43d42a8
Jonbn123 <42418062+Jonbn123@users.noreply.github.com>
2018-08-15 14:29:30 -0700

    Add files via upload

--- commands/reload.js
+++ commands/reload.js
@@ -1,13 +1,13 @@
 exports.run = (client, message, args, ops) => {
 
-if (message.author.id !== '244271175608303616') return message.channel.send('Sorry, only the owner can use this command.');
+    if (message.author.id !== '244271175608303616') return message.channel.send('Sorry, only the owner can use this command.');
 
-try {
+    try {
     delete require.cache[require.resolve(`./{args[0]}.js`)];
-}catch (e) {
+    }catch (e) {
     return message.channel.send(`unable to reload ${args[0]}`);
-}
+    }
 
-message.channel.send(`Successfully reload: ${args[0]}`);
+    message.channel.send(`Successfully reload: ${args[0]}`);
 
 }
