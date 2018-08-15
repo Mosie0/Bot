3df7a281dbe128c81469573aca8f480e9789dd58
Jonbn123 <42418062+Jonbn123@users.noreply.github.com>
2018-08-15 12:24:17 -0700

    Add files via upload

--- Starting.js
+++ Starting.js
@@ -5,8 +5,22 @@ const prefix = 'j!'
 const ownerID = '244271175608303616';
 
 client.on('message',nessage =>{
-let args = message.content.slice(prefix.length)
-});
+let args = message.content.slice(prefix.length).trim().split(' ');
+let cmd = args.shift().toLowerCase();
+if (message.author.bot)return;
+if (!message.content.startsWith(prefix)) return;
+try {
+
+delete require.cache[require.resolve(`./commands/${cmd}.js`)];
+
+let commandFile = require(`./commands/${cmd}.js`);
+commandFile.run(client, messagem, args);
+
+} catch (e) {
+console.log(e.stack);
+}
+
+ });
 
 client.on('ready', () =>console.log('Launched!'));
 
