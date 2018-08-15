dc0c2ab665102877a8b7384b1d1fd057479b8897
Jonbn123 <42418062+Jonbn123@users.noreply.github.com>
2018-08-15 13:06:02 -0700

    Add files via upload

--- Starting.js
+++ Starting.js
@@ -4,24 +4,24 @@ const client = new Discord.Client();
 const prefix = 'j!'
 const ownerID = '244271175608303616';
 
-client.on('message', message =>{
+client.on('message', message => {
     let args = message.content.slice(prefix.length).trim().split(' ');
     let cmd = args.shift().toLowerCase();
-    if (message.author.bot)return;
+    if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
     try {
 
-    delete require.cache[require.resolve(`./commands/${cmd}.js`)];
+        delete require.cache[require.resolve(`./commands/${cmd}.js`)];
 
-    let commandFile = require(`./commands/${cmd}.js`);
-    commandFile.run(client, messages, args);
+        let commandFile = require(`./commands/${cmd}.js`);
+        commandFile.run(client, messages, args);
 
     } catch (e) {
-    console.log(e.stack);
+        console.log(e.stack);
     }
 
  });
 
-client.on('ready', () =>console.log('Launched!'));
+client.on('ready', () => console.log('Launched!'));
 
 client.login(process.env.BOT_TOKEN);
