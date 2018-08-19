c1e97174dd1c461ad2804a26c6dcbee188790e2b
Jonbn123 <42418062+Jonbn123@users.noreply.github.com>
2018-08-19 11:19:45 -0700

    Add files via upload

--- Starting.js
+++ Starting.js
@@ -11,7 +11,118 @@ const serverStats = {
     botCountID: '480520548166664192'
 };
 
-client.on('message', message => {
+const db = require('quick.db');
+
+client.on('message', async message => {
+
+    if (message.autgor.bot) return;
+    if (message.channel.type !== 'text'){
+
+        let active = await db.fetch(`support_${message.author.id}`);
+
+        let guild = client.guilds.get('guildID');
+
+        let channel, found = true;
+
+        try {
+            if (active) client.channels.get(active.channelID).guild;
+        } catch (e) {
+            found = false;
+        }
+        if (!active || !found){
+
+            active = {};
+
+            channel = await guild/channels.create(`${message.author.username}-${message.author.discriminator}`, {
+                parent: `categoryID`,
+                topic: `?complete to close the ticket | Surpport for ${message.author.tag} | ID: ${message.author.id}`
+            });
+
+            let author = message.author;
+
+            const newChannel = new Discord.Message.Embed()
+                .setColor(0x36393e)
+                .setAuthor(author.tag, author.displayAvatarURL())
+                .setFooter('Support Ticket Created')
+                .addField('User', author)
+                .addField('ID', author.id)
+
+            await channel.send(newChannel);
+
+            const newTicket = new Discord.MessageEmbed()
+                .setColor(0x36393e)
+                .setAuthor(`Hello, ${author.tag}`, author.displayAvatarURL())
+                .setFooter('Support Ticket Created')
+
+                await author.send(newTicket);
+
+                active.channelID = channel.id;
+                active.targetID = author.id;
+        }
+
+            channel = client.channels.get(active.channelID);
+
+            const dm = new Discord.MessageEmbed()
+                .setColor(0x36393e)
+                .setAuthor(`Thank you, ${message.author.tag}`, message.author.displayAvatarURL())
+                .setFooter(`Your message has been sent -- A staff member will be in contact soon`)
+
+            await message.author.send(dm);
+
+            const embed = new Discord.MessageEmbed()
+                .setColor(0x36393e)
+                .setAuthor(message.author.tag, message.author.displayAvatarURL())
+                .setDescription(message.content)
+                .setFooter(`Message Recieved -- ${message.author.tag}`)
+
+            await channel.send(embed);
+
+            db.set(`support_${message.author.id}`, active);
+            db.set(`supportChannel_${channel.id}`, message.author.id);
+            return;
+
+        }
+
+    let support = await db.fetch(`supportChannel_${message.channel.id}`);
+
+    if (support) {
+
+            support = await db.fetch(`support_${support}`);
+
+        let supportUser = client.users.get(support.targetID);
+        if (!supportUser) return message.channel.delete();
+
+        if (message.content.toLowerCase() === '?complete') {
+
+            const complete = new Discord.MessageEmbed()
+                .setColor(0x36393e)
+                .setAuthor(`Hey, ${supportUser.tag}`, supportUser.displayAvatarURL())
+                .setFooter('Ticket Closed -- Jonbn123 Discord')
+                .setDescription('*Your ticket has been marked as **complete**. If you wish to reopen this, or create a new one, please send a message to the bot.')
+
+            supportUser.send(complete);
+
+            message.channel.delete();
+
+            db.delete(`support_${support.targetID}`);
+
+            }
+
+            const embed = new Discord.MessageEmbed()
+                .setColor(0x36393e)
+                .setAuthor(message.author.tag, message.author.displayAvatarURL())
+                .setFooter(`Message Recieved -- Jonbn123 Discord`)
+                .setDescription(message.content)
+
+                client.users.get(support.targetID).send(embed)
+
+                message.delete({timeout: 1000});
+
+                embed.setFooter(`Message Sent -- ${supportUser.tag}`).setDescription(message.content);
+
+                return message.channel.send(embed);
+        }
+
     let args = message.content.slice(prefix.length).trim().split(' ');
     let cmd = args.shift().toLowerCase();
     if (message.author.bot) return;
