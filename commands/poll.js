1eea95ecaf641cd62257d49dfa3ea06e5191b97b
Jonbn123 <42418062+Jonbn123@users.noreply.github.com>
2018-08-20 16:59:57 -0700

    Add files via upload

--- commands/poll.js
+++ commands/poll.js
@@ -2,11 +2,11 @@ const Discord = require('discord.js');
 
 exports.run = async (client, message, args, tools) => {
 
-    if (!message.member.roles.find(r => r.name === 'Owner')) return message.channel.send('This requires role: roleName');
+   // if (!message.member.roles.find(r => r.name === 'Owner')) return message.channel.send('This requires role: roleName');
 
     if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('This requires the permission: ADMINISTRATOR');
 
-   // if (!args[0]) return message.channel.send('Proper Usage: <prefix>poll question');
+    if (!args[0]) return message.channel.send('Proper Usage: <prefix>poll question');
 
     const embed = new Discord.MessageEmbed()
         .setColor(0xffffff)
