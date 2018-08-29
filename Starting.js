2fe9cc7ee3ea45d8bf018580478c9436ae5d7aae
Jonbn123 <42418062+Jonbn123@users.noreply.github.com>
2018-08-29 16:38:07 -0700

    Add files via upload

--- Starting.js
+++ Starting.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const prefix = 'j!'
+const prefix = 'J!'
 const ownerID = '244271175608303616';
 
 const serverStats = {
@@ -34,7 +34,7 @@ client.on('message', async message => {
  });
 
  client.on("ready", async () => {
-    client.user.setActivity(`s!help | I watch over ${client.guilds.size} Servers, and ${client.users.size} Users`, { type: "WATCHING" });
+    client.user.setActivity(`j!help | I watch over ${client.guilds.size} Servers, and ${client.users.size} Users`, { type: "WATCHING" });
  console.log("Launched!")
  });
 
@@ -57,12 +57,5 @@ client.on('guildMemberRemove', member =>{
     client.channels.get(serverStats.botCountID).setName(`Bot Count : ${member.guild.members.filter(m => m.user.bot).size}`);
 
 });
-
-    client.user.setPresence({
-    game: {
-        name: `j!help | I watch over ${client.guilds.size} Servers, and ${client.users.size} Users!`,
-        type: "WATCHING"
-    }
-})
 
 client.login(process.env.BOT_TOKEN);
